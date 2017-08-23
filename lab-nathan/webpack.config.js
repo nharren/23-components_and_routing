bf70e80061ad935950fd5f42fdc7af596ec7b560
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-08-23 12:44:48 -0700

    Configure project.

---
+++ lab-nathan/webpack.config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const HtmlPlugin = require('html-webpack-plugin');
+const ExtractPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  devtool: 'cheap-module-source-eval-map',
+  entry: `${__dirname}/src/main.js`,
+  output: {
+    path: `${__dirname}/build`,
+    filename: 'bundle-[hash].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlPlugin({ template: `${__dirname}/src/index.html` }),
+    new ExtractPlugin('bundle-[hash].css')
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractPlugin.extract(['css-loader', 'sass-loader'])
+      }
+    ]
+  }
+};
