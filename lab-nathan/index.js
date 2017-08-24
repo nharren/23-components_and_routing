9ff0526a776ee960e298e3de688cd03b611c7998
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-08-24 15:42:29 -0700

    Add minimum width to sidebar.

--- lab-nathan/index.js
+++ lab-nathan/index.js
@@ -4,28 +4,20 @@ const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let browserWindow
 
 function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
-  win.loadURL(url.format({
+  browserWindow = new BrowserWindow();
+  browserWindow.loadURL(url.format({
     pathname: path.join(__dirname, '/build/index.html'),
     protocol: 'file:',
     slashes: true
   }))
-
-  // Open the DevTools.
-  win.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  win.on('closed', () => {
+  browserWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
+    browserWindow = null
   })
 }
 
@@ -46,10 +38,7 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (win === null) {
+  if (browserWindow === null) {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
